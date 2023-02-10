48252390ca5b5fb24eb025fe95aaacd181cb7dd2
wuyefa <648715162@qq.com>
2023-02-10 09:00:22 +0800

    开始写聚类

--- models/CLUSTER/cluster.py
+++ models/CLUSTER/cluster.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class Cluster(nn.Module):
     
-    def __init__(self,  p_dropout, leaky_c=0.2, final_out_noden=22, input_feature=35, **dic):
+    def __init__(self, p_dropout, leaky_c=0.2, final_out_noden=22, input_feature=35, **dic):
         super(Cluster, self).__init__()
         
         
