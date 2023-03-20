a3690632137a1e2f17131a9b69665e7735846294
wuyefa <648715162@qq.com>
2023-03-20 10:45:19 +0800

    给模型添加选点功能

--- models/GISLR/test.py
+++ models/GISLR/test.py
@@ -1,19 +1,29 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
 class Model(nn.Module):
-    def __init__(self, **dic):
+    def __init__(self, block_type="right_hand", **dic):
         super(Model, self).__init__()
+        self.block_type = block_type
+        self.face_nb = np.arange(0, 468).tolist()
+        self.lhand_nb = np.arange(468, 489).tolist()
+        self.pose_nb = np.arange(489, 522).tolist()
+        self.rhand_nb = np.arange(522, 543).tolist()
+        self.nb_type = {'face':self.face_nb, 'left_hand':self.lhand_nb, 'pose':self.pose_nb, 'right_hand':self.rhand_nb}
+        nb_v = len(self.nb_type[block_type])
         self.layer1 = nn.Linear(3, 128)
         self.layer2 = nn.Linear(128, 64)
         self.layer3 = nn.Linear(64, 32)
         self.layer4 = nn.Linear(32, 16)
-        self.layer5 = nn.Linear(16 * 543, 250)
+        self.layer5 = nn.Linear(16 * nb_v, 250)
         self.relu = nn.ReLU()
         self.flatten = nn.Flatten(start_dim=2, end_dim=-1)
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
+        x = x[:, :, self.nb_type[self.block_type], :]
+        print(x.shape)
         x = self.relu(self.layer1(x))
         x = self.relu(self.layer2(x))
         x = self.relu(self.layer3(x))
